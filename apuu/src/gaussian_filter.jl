6124f250fef103973946978685a7ba9fbe8289df
JonathanSomer <jonathan.somer@protonmail.com>
2025-12-05 03:07:24 +0200

    [HW3 hands on done]

--- apuu/src/gaussian_filter.jl
+++ apuu/src/gaussian_filter.jl
@@ -1,6 +1,6 @@
 module GaussianFilter
 
-export PropagateBelief, PosteriorBeliefRangedBeacons
+export PropagateBelief, PosteriorBeliefRangedBeacons, PosteriorBeliefBeacons
 
 using apuu.RobotEnv2D: POMDPscenario
 using Distributions
@@ -32,27 +32,29 @@ Compute the posterior belief given a prior belief, action, and ranged beacon obs
 - `𝒫::POMDPscenario`: The POMDP scenario
 - `b::FullNormal`: The prior belief (Gaussian)
 - `a::Vector{Float64}`: The action taken
-- `z::NamedTuple`: The observation, containing `obs` and `beacon_index`
+- `z::NamedTuple`: The relative observation, containing `obs` and `beacon_index`
 """
 function PosteriorBeliefRangedBeacons(𝒫::POMDPscenario, b::FullNormal, a::Vector{Float64}, z::NamedTuple)::FullNormal
+
+    beacon_index = z.beacon_index
+    zrel = z.obs
+
     # predicted mean and variance:
     μp = b.μ + a
     Σp = b.Σ + 𝒫.Σw
 
     # observation covariance:
-    r = norm(μp - 𝒫.beacons[z.beacon_index, :])
+    r = norm(zrel)
     Σv = 0.1^2 * max(r, 𝒫.rmin)^2 * I(2)
 
     # shift relative observation to beacon position:
-    z = z.obs + 𝒫.beacons[z.beacon_index, :]
+    z_abs = zrel + 𝒫.beacons[beacon_index, :]
     
     # Kalman gain:
     K = Σp * inv(Σp + Σv)
 
-    # println("Kalman Gain K: ", K)
-
     # updated mean and covariance:
-    μb′ = (I(2) - K) * μp + K * z
+    μb′ = (I(2) - K) * μp + K * z_abs
     Σb′ = (I(2) - K) * Σp
 
     return MvNormal(μb′, Σb′)
@@ -76,7 +78,6 @@ function PosteriorBeliefBeacons(𝒫::POMDPscenario, b::FullNormal, a::Vector{Fl
     Σp = b.Σ + 𝒫.Σw
 
     # observation covariance:
-    r = norm(μp - 𝒫.beacons[z.beacon_index, :])
     Σv = 0.1^2 * I(2)
 
     # shift relative observation to beacon position:
